bbf28abe88c17ba759eeafd32d32e6ddf1218223
Murat Özkorkmaz <murat.oezkorkmaz@gmail.com>
2020-01-03 11:07:29 +0100

    Update RatioSaveCommand.php

--- Command/RatioSaveCommand.php
+++ Command/RatioSaveCommand.php
@@ -1,7 +1,7 @@
 <?php
 namespace Tbbc\MoneyBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,7 +12,7 @@ use Tbbc\MoneyBundle\Pair\PairManagerInterface;
  * Class RatioSaveCommand
  * @package Tbbc\MoneyBundle\Command
  */
-class RatioSaveCommand extends ContainerAwareCommand
+class RatioSaveCommand extends Command
 {
     protected static $defaultName = 'tbbc:money:ratio-save';
     
